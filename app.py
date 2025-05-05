0d8203a0668388e444609fc46fd20834144c5beb
jakolling <jakolling@gmail.com>
2025-05-05 21:51:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -187,14 +187,7 @@ def create_pizza_chart(params=None, values_p1=None, values_p2=None, values_avg=N
             kwargs_slices=dict(
                 edgecolor="#F2F2F2", zorder=2, linewidth=1
             ),
-            kwargs_params=dict(
-                color="#000000",
-                fontsize=8,
-                fontweight="normal",
-                va="center",
-                zorder=3,
-                fontproperties=roboto_condensed.prop
-            ),
+            kwargs_params=dict(),
             kwargs_values=dict(
                 color="#FF0000",
                 fontsize=8,
@@ -362,14 +355,7 @@ def create_comparison_pizza_chart(params, values_p1, values_p2=None, values_avg=
                 facecolor=compare_color, edgecolor="#000000", 
                 zorder=3, linewidth=1, alpha=0.8
             ),
-            kwargs_params=dict(
-                color="#000000",
-                fontsize=8,
-                fontweight="normal",
-                va="center",
-                zorder=3,
-                fontproperties=roboto_condensed.prop
-            ),
+            kwargs_params=dict(),
             kwargs_values=dict(
                 color=player1_color,
                 fontsize=8,
